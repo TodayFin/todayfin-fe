37c6934ed3c282a5574eb03e7160d0ca28960787
s1lv3rrud <lek8575@naver.com>
2024-07-18 16:34:38 +0900

    feat: 환율 컴포넌트에 날짜 추가

--- src/app/components/ExchangeRate.js
+++ src/app/components/ExchangeRate.js
@@ -1,4 +1,5 @@
 const ExchangeRate = () => {
+  const date = "2024-07-18";
   const exchangeRates = [
     { currency: "🇺🇸 USD", rate: "1,200" },
     { currency: "💶 EUR", rate: "1,350" },
@@ -11,11 +12,14 @@ const ExchangeRate = () => {
     <div className="p-4 bg-white shadow-md rounded-lg">
       <div className="flex justify-between items-center mb-4">
         <h2 className="text-xl font-bold">💱 환율</h2>
-        <span className="text-gray-500 text-sm cursor-pointer">더보기</span>
+        <span className="text-gray-500 text-sm cursor-pointer">{date}</span>
       </div>
       <div className="grid grid-cols-2 gap-4">
         {exchangeRates.map((rate, index) => (
-          <div key={index} className="bg-yellow-100 rounded-md p-2 flex items-center justify-between">
+          <div
+            key={index}
+            className="bg-yellow-100 rounded-md p-2 flex items-center justify-between"
+          >
             <p className="text-lg font-semibold mr-2">{rate.currency}</p>
             <p className="text-lg">{rate.rate}</p>
           </div>
