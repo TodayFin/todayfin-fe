fe67e2e9d10409cce7c28f18e9e2cb037c440a49
s1lv3rrud <lek8575@naver.com>
2024-07-23 15:29:50 +0900

    feat: 전역 타이틀 추가

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,10 @@ import Header from "../components/layout/Header";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="logo.ico" />
+        <title>TodayFin</title>
+      </head>
       <body>
         <Header />
         {children}
