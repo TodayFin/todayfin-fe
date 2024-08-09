275cffa48b1ded00922ab5b1375702b2c5dceb02
s1lv3rrud <lek8575@naver.com>
2024-08-09 15:19:12 +0900

    chore: 뉴스 mockData 변경

--- src/components/news/NewsContent.js
+++ src/components/news/NewsContent.js
@@ -4,7 +4,13 @@ import Image from "next/image";
 const NewsContent = ({ imageSrc, content }) => {
   return (
     <div className="mb-8">
-      <Image src={imageSrc} alt="News" className="w-full h-auto mb-4" />{" "}
+      <Image
+        src={imageSrc}
+        alt="News"
+        className="w-full h-auto mb-4"
+        width={48}
+        height={48}
+      />{" "}
       <div className="text-lg text-gray-800">{content}</div>
     </div>
   );
