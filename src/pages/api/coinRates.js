abadf3d47a3e10719ea7d48388632793be337521
s1lv3rrud <lek8575@naver.com>
2024-08-27 16:43:21 +0900

    feat: coins API 라우팅

--- src/pages/api/coinRates.js
+++ src/pages/api/coinRates.js
@@ -1,63 +1,20 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
 const fetchCoinRates = async (req = NextApiRequest, res = NextApiResponse) => {
-  const apiKey = process.env.ALPHA_VANTAGE_API_KEY;
-  const currencies = [
-    "BTC",
-    "ETH",
-    "BNB",
-    "XRP",
-    "ADA",
-    "SOL",
-    "DOT",
-    "DOGE",
-    "LINK",
-    "LTC",
-    "AAVE",
-    "UNI",
-    "BCH",
-    "XLM",
-    "ATOM",
-    "XMR",
-    "AVAX",
-    "TRX",
-  ];
-  const toCurrency = "KRW";
+  const backendUrl = process.env.ALPHA_VANTAGE_BACKEND_URL;
+
+  const apiUrl = `${backendUrl}/coins`;
 
   try {
-    const promises = currencies.map((currency) =>
-      fetch(
-        `https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=${currency}&to_currency=${toCurrency}&apikey=${apiKey}`
-      ).then((response) => response.json())
-    );
+    const response = await fetch(apiUrl);
+    const data = await response.json();
 
-    const results = await Promise.all(promises);
-
-    const coinRates = results.map((result) => {
-      const coinRateData = result["Realtime Currency Exchange Rate"];
-      if (coinRateData) {
-        return {
-          name: coinRateData["2. From_Currency Name"],
-          rate: parseFloat(coinRateData["5. Exchange Rate"]).toFixed(2),
-          last_refreshed: coinRateData["6. Last Refreshed"],
-          bid: parseFloat(coinRateData["8. Bid Price"]).toFixed(2),
-          ask: parseFloat(coinRateData["9. Ask Price"]).toFixed(2),
-        };
-      } else {
-        return {
-          name: "API limit exceeded",
-          rate: "-1",
-          last_refreshed: "N/A",
-          bid: "-1",
-          ask: "-1",
-        };
-      }
-    });
-
-    res.status(200).json(coinRates);
+    if (data.coins) {
+      res.status(200).json(data.coins);
+    } else {
+      res.status(500).json({ error: "Unexpected response format from API" });
+    }
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Error fetching exchange rates" });
+    console.error("Error fetching coin rates:", error);
+    res.status(500).json({ error: "Internal server error" });
   }
 };
 
