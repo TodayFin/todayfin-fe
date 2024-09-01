338d9534376a80db010320f4940ab420310e5188
s1lv3rrud <lek8575@naver.com>
2024-09-01 14:36:21 +0900

    feat: 커뮤니티 전체 글 열람 라우팅

--- src/app/community/page.js
+++ src/app/community/page.js
@@ -9,75 +9,31 @@ const CommunityPage = () => {
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const newPosts = [
-        {
-          id: 1,
-          userName: "Daniel",
-          userId: "danielwiden",
-          content:
-            "I've built pretty handy sites powered by Craft or WordPress in the past, but seeing @framer tackle CMS stuff so effortlessly is mind-boggling I've built pretty handy sites powered by Craft or WordPress in the past, but seeing @framer tackle CMS stuff so effortlessly is mind-boggling I've built pretty handy sites powered by Craft or WordPress in the past, but seeing @framer tackle CMS stuff so effortlessly is mind-boggling",
-        },
-        {
-          id: 2,
-          userName: "Dann",
-          userId: "dannpetty",
-          content: "I learned how to build a website in @Framer!",
-        },
-        {
-          id: 3,
-          userName: "Manoj",
-          userId: "manojyjack",
-          content: "Websites built on @framer look so beautiful.",
-        },
-        {
-          id: 4,
-          userName: "Fekry",
-          userId: "fekryjack",
-          content:
-            "I have been in an @framer rabbit hole for the past 8 hours without even noticing as time goes by — its everything I love in Figma combined with everything I love",
-        },
-        {
-          id: 5,
-          userName: "Davo",
-          userId: "pixelboat",
-          content:
-            "A quick word about @framer. Framer is forever changing the experience of designing for the web. Forget about a design tool. Once you get the basics, you're",
-        },
-        {
-          userName: "Miguel",
-          userId: "mgvslv",
-          content:
-            "Learned some basics of @framer yesterday, and today I delivered a landing page for a client. It’s so unreal how small the learning curve is from Figma to @framer.",
-        },
-        {
-          userName: "Mani",
-          userId: "ogamyar37",
-          content:
-            "The scroll variant in @framer hits differently. Never imaged that making some complex things would be easy with that. Everyday i learn something new",
-        },
-        {
-          id: 6,
-          userName: "Parker",
-          userId: "pl_parker",
-          content:
-            "I was enjoying @framer a lot but I am BLOWN AWAY by their Figma plug-in. From Auto-layout to flex-box in the browser in seconds; this has completely changed how",
-        },
-        {
-          id: 7,
-          userName: "Lauren",
-          userId: "write_tacos",
-          content:
-            "Honestly the @framer publish time is insanely fast. Just published 2 weeks of changes in 5 seconds – like it’s almost too fast for such a big moment.",
-        },
-        {
-          userName: "Daniel",
-          userId: "danielwiden",
-          content:
-            "I've built pretty handy sites powered by Craft or WordPress in the past, but seeing @framer tackle CMS stuff so effortlessly is mind-boggling",
-        },
-      ];
+      try {
+        const response = await fetch(`/api/community?page=${page}`, {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+          },
+        });
 
-      setPosts((prevPosts) => [...prevPosts, ...newPosts]);
+        if (!response.ok) {
+          throw new Error("Failed to fetch posts");
+        }
+
+        const data = await response.json();
+
+        const newPosts = data.posts.map((post) => ({
+          id: post._id,
+          userName: `User ${post.authorId}`,
+          userId: `user${post.authorId}`,
+          content: post.content,
+        }));
+
+        setPosts((prevPosts) => [...prevPosts, ...newPosts]);
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+      }
     };
 
     fetchPosts();
---
+++ src/pages/api/community.js
@@ -0,0 +1,33 @@
+const fetchCommunityPosts = async (req, res) => {
+  const { page } = req.query;
+  const apiKey = process.env.BACKEND_URL;
+  const authorizationHeader = req.headers.authorization;
+
+  if (!authorizationHeader) {
+    return res.status(401).json({ error: "Authorization header is missing" });
+  }
+
+  try {
+    const response = await fetch(`${apiKey}/community?page=${page}`, {
+      method: "GET",
+      headers: {
+        Authorization: authorizationHeader,
+      },
+    });
+
+    if (!response.ok) {
+      return res
+        .status(response.status)
+        .json({ error: "Failed to fetch posts" });
+    }
+
+    const data = await response.json();
+
+    res.status(200).json(data);
+  } catch (error) {
+    console.error("Error fetching community posts:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export default fetchCommunityPosts;
